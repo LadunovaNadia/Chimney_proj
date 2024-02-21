b2c811f515c2c62a65d6d4603b8ad39047556821
LadunovaNadia <ladunova.nadia@gmail.com>
2024-02-21 18:36:35 +0200

    update_images

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import '../src/partials/header/header';
+import './partials/main/main';
+import './partials/conus_chimney/conus_chimney';
+import './partials/cream_inside/cream_inside';
+import './partials/footer/footer';
+import './partials/menu/menu';
+import './partials/salted_chimney/salted.chimney';
+import './partials/main/main';
